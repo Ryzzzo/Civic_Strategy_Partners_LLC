03265f92fb2cb41cbd3bc5ed8bfa7b516beace11
Ryzzzo <r.stacy@me.com>
2025-11-23 16:37:51 -0500

    Updated page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -38,11 +38,6 @@ export default function Home() {
   const [gsaNews, setGsaNews] = useState<GSANewsItem[]>([]);
   const [gsaNewsLoading, setGsaNewsLoading] = useState(true);
   const [gsaNewsError, setGsaNewsError] = useState(false);
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -172,10 +167,6 @@ export default function Home() {
       sections.forEach((section) => observer.unobserve(section));
     };
   }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   return (
     <>
